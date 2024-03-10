30e3697fa078449a0dda26de2bb77f60117c40a9
Iamsarafatima <162489179+Iamsarafatima@users.noreply.github.com>
2024-03-10 05:47:11 -0700

    Add files via upload

---
+++ ass10.ts
@@ -0,0 +1,3 @@
+//Ex: 10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//let famous_person: string = "Albert Einstien";
+//let message: string = 'Albert Einstien once said: "A person who never made a mistake never tried anything new.";'
